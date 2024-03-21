b3989aa0f76977226ac4125a98518c5783261b3a
rafacat <simiaopereira3@gmail.com>
2024-03-20 22:31:12 -0300

    conhecendo o cast e alguns tipos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        /*
+         * Para atribuir um variavel que possue um armazenamento maior que a outra
+         * Nós utilizamos o "cast" mas, é necessario tomar muito cuidado para
+         * utiliza-lo pois, é uma ferramenta perigosa.
+         *
+         * Cast - Utilizado para atribuir um armazenamento de dado superior em um
+         * inferior.
+         */
     }
 }
