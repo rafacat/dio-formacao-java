5cd5225fd03ea203d7f728deb57778b53b37dfe4
rafacat <simiaopereira3@gmail.com>
2024-06-19 22:57:51 -0300

    Estudo de condicao simples

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,30 +1,12 @@
-import java.util.Scanner;
-import java.util.Locale;
-
 public class CaixaEletronico {
+    public static void main(String[] args) throws Exception {
 
-   public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
 
-      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
 
-
-      System.out.println("Informe o saldo de sua conta: ");
-      double saldo = scanner.nextDouble();
-
-      System.out.println("Informe o valor a ser sacado: ");
-      double valorSolicitado = scanner.nextDouble();
-
-      if(valorSolicitado < saldo){
-         saldo = saldo - valorSolicitado;
-         System.out.println("o valor a ser sacado é R$ " + valorSolicitado);
-         System.out.println("E seu saldo atual é R$ " + saldo);
-
-      } else {
-         System.out.println("Saldo insuficente");
-      }
-
-
-      System.out.println("Muito obrigado por utilizar nossos serviços.");
-
-   }
+        System.out.println(saldo);
+    }
 }
