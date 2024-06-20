b6edace427677f08298cff83777abdeca4890048
rafacat <simiaopereira3@gmail.com>
2024-06-19 23:18:05 -0300

    condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,15 @@
 public class ResultadoEscolar {
    public static void main(String[] args) {
 
-      int nota = 9;
+      int nota = 6;
 
       if(nota >= 7)
          System.out.println("Aprovado!");
 
-      else
-         System.out.println("Reprovado :(");
+         else if(nota >= 5 && nota < 7)
+         System.out.println("Prova de recuperação");
+
+         else
+            System.out.println("Reprovado :(");
    }
 }
