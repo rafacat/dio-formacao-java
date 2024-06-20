1cbfc7e66cf6761852dc8c8befbe6239ad7902fb
rafacat <simiaopereira3@gmail.com>
2024-06-19 23:29:36 -0300

    condicao ternaria

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,14 +2,8 @@ public class ResultadoEscolar {
    public static void main(String[] args) {
 
       int nota = 6;
+      String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && < 7 ? "Recuperação" : "Reprovado";
 
-      if(nota >= 7)
-         System.out.println("Aprovado!");
-
-         else if(nota >= 5 && nota < 7)
-         System.out.println("Prova de recuperação");
-
-         else
-            System.out.println("Reprovado :(");
+      System.out.println(resultado);
    }
 }
