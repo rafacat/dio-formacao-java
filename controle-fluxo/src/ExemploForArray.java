2dcf95df02de537324f5a30dd7fee7e51a93f977
rafacat <simiaopereira3@gmail.com>
2024-06-25 23:42:29 -0300

    Estrutura de repeticao For

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+   public static void main(String[] args) {
+      String alunos [] = {"RAFAEL", "JORGE", "MATHEUS", "RENATA"};
+
+      for (int x=0; x<=alunos.length; x++ ) {
+         System.out.println("O aluno do indice x=" + x + " é " + alunos[x]);
+      }
+   }
+}
